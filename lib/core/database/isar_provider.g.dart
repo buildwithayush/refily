7bfc7e6d595a0e79e178264cc1f05bc5e2c8f507
Ayush Umrao <umraoayush@gmail.com>
2026-07-25 01:45:15 +0530

    feat(isar): set up Isar database and global provider

---
+++ lib/core/database/isar_provider.g.dart
@@ -0,0 +1,50 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'isar_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(isar)
+final isarProvider = IsarProvider._();
+
+final class IsarProvider extends $FunctionalProvider<Isar, Isar, Isar>
+    with $Provider<Isar> {
+  IsarProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'isarProvider',
+        isAutoDispose: false,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$isarHash();
+
+  @$internal
+  @override
+  $ProviderElement<Isar> $createElement($ProviderPointer pointer) =>
+      $ProviderElement(pointer);
+
+  @override
+  Isar create(Ref ref) {
+    return isar(ref);
+  }
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(Isar value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<Isar>(value),
+    );
+  }
+}
+
+String _$isarHash() => r'6013e20ce8cc6311a8437b538fc3f9419e75da5a';
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:refily/core/database/isar_database.dart';
+import 'package:refily/core/database/isar_provider.dart';
 import 'package:refily/core/providers/theme_provider.dart';
 import 'package:refily/core/router/app_router.dart';
 import 'package:refily/core/services/supabase_service.dart';
@@ -11,12 +13,16 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   final prefs = await SharedPreferences.getInstance();
+  final isar = await IsarDatabase.initialize();
 
   await dotenv.load(fileName: ".env");
   await SupabaseService.initialize();
   runApp(
     ProviderScope(
-      overrides: [sharedPreferencesProvider.overrideWithValue(prefs)],
+      overrides: [
+        sharedPreferencesProvider.overrideWithValue(prefs),
+        isarProvider.overrideWithValue(isar),
+      ],
 
       child: const MyApp(),
     ),
