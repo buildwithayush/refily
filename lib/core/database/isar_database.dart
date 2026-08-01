d58b2f7bec954ce7b668196e71271c94d46ddec8
Ayush Umrao <umraoayush@gmail.com>
2026-08-01 09:14:38 +0530

    feat(categories): add Isar model and register database schema

--- lib/core/database/isar_database.dart
+++ lib/core/database/isar_database.dart
@@ -1,5 +1,6 @@
 import 'package:isar_plus/isar_plus.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:refily/features/categories/domain/models/category_isar_model.dart';
 import 'package:refily/features/product/data/models/product_isar_model.dart';
 import 'package:refily/features/wishlist/data/models/wishlist_isar_model.dart';
 
@@ -7,7 +8,11 @@ class IsarDatabase {
   static Future<Isar> initialize() async {
     final dir = await getApplicationDocumentsDirectory();
     return await Isar.openAsync(
-      schemas: [WishlistIsarModelSchema,ProductIsarModelSchema],
+      schemas: [
+        WishlistIsarModelSchema,
+        ProductIsarModelSchema,
+        CategoryIsarModelSchema,
+      ],
       directory: dir.path,
     );
   }
