c360d1c4b44cabb67a74b028ef532b40d6fffdc0
Ayush Umrao <umraoayush@gmail.com>
2026-08-20 20:52:07 +0530

    feat(auth): implement routing and state management for auth flow

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,5 +1,8 @@
 import 'package:go_router/go_router.dart';
 import 'package:refily/core/router/app_routes.dart';
+import 'package:refily/features/auth/presentation/providers/auth_controller.dart';
+import 'package:refily/features/auth/presentation/screens/login_screen.dart';
+import 'package:refily/features/auth/presentation/screens/signUp_screen.dart';
 import 'package:refily/features/cart/presentation/screens/cart_screen.dart';
 import 'package:refily/features/categories/presentation/screens/categories_product_screen.dart';
 import 'package:refily/features/categories/presentation/screens/categories_screen.dart';
@@ -9,12 +12,50 @@ import 'package:refily/features/profile/presentation/screens/profile_screen.dart
 import 'package:refily/features/settings/presentation/screens/setting_screen.dart';
 import 'package:refily/features/shared/presentation/screens/bottom_naviagtion_screen.dart';
 import 'package:refily/features/wishlist/presentation/screens/wishlist_screen.dart';
-// import 'package:refily/features/wishlist/presentation/screens/wishlist_screen.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-class AppRouter {
-  static final GoRouter goRouter = GoRouter(
+part 'app_router.g.dart';
+
+@riverpod
+GoRouter routerProvider(Ref ref) {
+  final authStateAsync = ref.watch(authStateProviderProvider);
+
+  return GoRouter(
     initialLocation: AppRoutes.home,
+
+  
+    redirect: (context, state) {
+      
+      if (authStateAsync.isLoading) return null;
+
+      final authState = authStateAsync.value;
+      final isAuthenticated = authState?.session != null;
+      final isGoingToAuthScreen =
+          state.matchedLocation == AppRoutes.login ||
+          state.matchedLocation == AppRoutes.signup;
+
+     
+      if (!isAuthenticated && !isGoingToAuthScreen) {
+        return AppRoutes.login;
+      }
+
+      
+      if (isAuthenticated && isGoingToAuthScreen) {
+        return AppRoutes.home;
+      }
+
+      return null;
+    },
+
     routes: <RouteBase>[
+      GoRoute(
+        path: AppRoutes.login,
+        builder: (context, state) => const LoginScreen(),
+      ),
+      GoRoute(
+        path: AppRoutes.signup,
+        builder: (context, state) => const SignupScreen(),
+      ),
       ShellRoute(
         builder: (context, state, child) {
           return BottomNaviagtionScreen(child: child);
@@ -28,7 +69,6 @@ class AppRouter {
             path: AppRoutes.categoryProduct,
             builder: (context, state) {
               final categoryId = state.extra as int;
-
               return CategoryProductsScreen(categoryId: categoryId);
             },
           ),
