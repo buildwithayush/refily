316807840ed2553b177d0dd053cc3437ae679dde
Ayush Umrao <umraoayush@gmail.com>
2026-08-22 09:10:21 +0530

    fix(ci): resolve analyzer issues

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -2,7 +2,7 @@ import 'package:go_router/go_router.dart';
 import 'package:refily/core/router/app_routes.dart';
 import 'package:refily/features/auth/presentation/providers/auth_controller.dart';
 import 'package:refily/features/auth/presentation/screens/login_screen.dart';
-import 'package:refily/features/auth/presentation/screens/signUp_screen.dart';
+import 'package:refily/features/auth/presentation/screens/signup_screen.dart';
 import 'package:refily/features/cart/presentation/screens/cart_screen.dart';
 import 'package:refily/features/categories/presentation/screens/categories_product_screen.dart';
 import 'package:refily/features/categories/presentation/screens/categories_screen.dart';
